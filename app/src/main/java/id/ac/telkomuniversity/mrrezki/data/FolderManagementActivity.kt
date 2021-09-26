a3a6bcefbf4006873a85c7a4e6cbc5546b1fcdcc
nando <muhammad.rezki.ananda@gmail.com>
2021-09-26 15:16:02 +0700

    Create and Delete Folder

--- app/src/main/java/id/ac/telkomuniversity/mrrezki/data/FolderManagementActivity.kt
+++ app/src/main/java/id/ac/telkomuniversity/mrrezki/data/FolderManagementActivity.kt
@@ -1,10 +1,21 @@
 package id.ac.telkomuniversity.mrrezki.data
 
 import android.os.Bundle
+import android.os.Environment
 import androidx.appcompat.app.AppCompatActivity
 import id.ac.telkomuniversity.mrrezki.databinding.ActivityFolderManagementBinding
+import id.ac.telkomuniversity.mrrezki.utils.showToast
+import java.io.File
 
 class FolderManagementActivity : AppCompatActivity() {
+
+    companion object {
+        const val FOLDER_NAME = "New Folder"
+        const val SUCCESS_CREATED = "Success create $FOLDER_NAME"
+        const val FOLDER_EXISTS = "Folder already exists"
+        const val SUCCESS_DELETED = "Success deleted $FOLDER_NAME"
+        const val FOLDER_NOT_FOUND = "Folder not found"
+    }
 
     private val binding: ActivityFolderManagementBinding by lazy {
         ActivityFolderManagementBinding.inflate(
@@ -15,5 +26,39 @@ class FolderManagementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupView()
+    }
+
+    private fun setupView() {
+        val downloadsDirectory =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        binding.btnCreateFolder.setOnClickListener {
+            createFolder(downloadsDirectory)
+        }
+        binding.btnDeleteFolder.setOnClickListener {
+            deleteFolder(downloadsDirectory)
+        }
+    }
+
+    private fun createFolder(directory: File) {
+        val file = File(directory, FOLDER_NAME)
+
+        if (!file.exists()) {
+            file.mkdir()
+            showToast(SUCCESS_CREATED)
+        } else {
+            showToast(FOLDER_EXISTS)
+        }
+    }
+
+    private fun deleteFolder(directory: File) {
+        val file = File(directory, FOLDER_NAME)
+
+        if (file.exists()) {
+            file.delete()
+            showToast(SUCCESS_DELETED)
+        } else {
+            showToast(FOLDER_NOT_FOUND)
+        }
     }
 }
