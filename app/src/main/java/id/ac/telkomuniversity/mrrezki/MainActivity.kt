ac82a75aa4a97fc37d09b77f31e7a2d4253858d0
nando <muhammad.rezki.ananda@gmail.com>
2021-09-26 13:52:18 +0700

    initPermissions

--- app/src/main/java/id/ac/telkomuniversity/mrrezki/MainActivity.kt
+++ app/src/main/java/id/ac/telkomuniversity/mrrezki/MainActivity.kt
@@ -1,11 +1,47 @@
 package id.ac.telkomuniversity.mrrezki
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initPermissions()
+    }
+
+    private fun initPermissions() {
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
+                    if (p0!!.areAllPermissionsGranted()) {
+                        //Timber.d("All permissions are granted")
+                    } else {
+                        val intent: Intent = intent
+                        finish()
+                        startActivity(intent)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: MutableList<PermissionRequest>?,
+                    p1: PermissionToken?
+                ) {
+                    p1!!.continuePermissionRequest()
+                }
+
+            }).withErrorListener {
+                //Timber.d("$it")
+            }.check()
     }
 }
